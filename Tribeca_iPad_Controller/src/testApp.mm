56fe66e10dd5121e1e2bdfe4a83b7af5d51f5457
Ben McChesney <ben.mcchesney@gmail.com>
2013-03-22 11:46:31 -0700

    Minor iOS changes.

--- Tribeca_iPad_Controller/src/testApp.mm
+++ Tribeca_iPad_Controller/src/testApp.mm
@@ -12,7 +12,9 @@ void testApp::setup(){
 	arrow.loadImage("arrow.png");
 	arrow.setAnchorPercent(1.0, 0.5);
     
-    sender.setup( "localHost"  , 98765 );
+    host = "192.168.161.87" ;
+    port = 12345 ; 
+    sender.setup( host , port );
 }
 
 
@@ -25,12 +27,15 @@ void testApp::update()
 		m.addIntArg( ofGetFrameNum() );
 		sender.sendMessage( m );
 	}
-    ofxOscMessage m ;
-    m.setAddress( "accel" ) ;
-    m.addFloatArg(  ofxAccelerometer.getForce().x ) ;
-    m.addFloatArg(  ofxAccelerometer.getForce().y ) ;
-    sender.sendMessage( m ) ; 
-//    oscSender.sendMessage( m ) ;
+    ofxOscMessage m2 ;
+    m2.setAddress( "/accel" ) ;
+    m2.addFloatArg(  ofxAccelerometer.getForce().x ) ;
+    m2.addFloatArg(  ofxAccelerometer.getForce().y ) ;
+    sender.sendMessage( m2 ) ;
+    
+    
+   // cout << "SENDING DATA " << endl ;
+//    sender.sendMessage( m ) ;
     /*
     ofLog(OF_LOG_VERBOSE, "x = %f, y = %f", ofxAccelerometer.getForce().x, ofxAccelerometer.getForce().y);
      */
@@ -39,15 +44,35 @@ void testApp::update()
 //--------------------------------------------------------------
 void testApp::draw() {
 	ofSetColor(54);
-	ofDrawBitmapString("Multitouch and Accel Example", 10, 20);
+	ofDrawBitmapString("SENDING ON : "+ host + ":" + ofToString( port ) , 10, 20);
 
 	float angle = 180 - RAD_TO_DEG * atan2( ofxAccelerometer.getForce().y, ofxAccelerometer.getForce().x );
 
+    float xForce = ofxAccelerometer.getForce().x ;
+    float yForce = ofxAccelerometer.getForce().y ;
+    if ( abs(xForce) > abs(yForce) )
+    {
+        yForce = xForce ;
+    }
+    
+    
+    
+    float mappedScale = ofMap ( abs(yForce) , -1 , 1 , -7 , 7 ) ;
+    
+    if ( abs( mappedScale) < 1.5 )
+    {
+        if ( mappedScale < 0 )
+            mappedScale = -1.5 ;
+        else
+            mappedScale = 1.5 ;
+    }
+    
 	ofEnableAlphaBlending();
 	ofSetColor(255);
 	ofPushMatrix();
 		ofTranslate(ofGetWidth()/2, ofGetHeight()/2, 0);
 		ofRotateZ(angle);
+        ofScale( mappedScale , mappedScale ) ; 
 		arrow.draw(0,0);
 	ofPopMatrix();
 }
@@ -59,22 +84,22 @@ void testApp::exit(){
 
 //--------------------------------------------------------------
 void testApp::touchDown(ofTouchEventArgs & touch){
-    ofLog(OF_LOG_VERBOSE, "touch %d down at (%d,%d)", touch.id, touch.x, touch.y);
+    //ofLog(OF_LOG_VERBOSE, "touch %d down at (%d,%d)", touch.id, touch.x, touch.y);
 }
 
 //--------------------------------------------------------------
 void testApp::touchMoved(ofTouchEventArgs & touch){
-    ofLog(OF_LOG_VERBOSE, "touch %d moved at (%d,%d)", touch.id, touch.x, touch.y);
+    //ofLog(OF_LOG_VERBOSE, "touch %d moved at (%d,%d)", touch.id, touch.x, touch.y);
 }
 
 //--------------------------------------------------------------
 void testApp::touchUp(ofTouchEventArgs & touch){
-    ofLog(OF_LOG_VERBOSE, "touch %d up at (%d,%d)", touch.id, touch.x, touch.y);
+    //ofLog(OF_LOG_VERBOSE, "touch %d up at (%d,%d)", touch.id, touch.x, touch.y);
 }
 
 //--------------------------------------------------------------
 void testApp::touchDoubleTap(ofTouchEventArgs & touch){
-    ofLog(OF_LOG_VERBOSE, "touch %d double tap at (%d,%d)", touch.id, touch.x, touch.y);
+    //ofLog(OF_LOG_VERBOSE, "touch %d double tap at (%d,%d)", touch.id, touch.x, touch.y);
 }
 
 //--------------------------------------------------------------
